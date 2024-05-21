b8c83630096f61a1b90dced1c657e60a4dfd9aff
T.M.M <yahyotojiboyev122@gmail.com>
2024-05-21 07:41:44 +0500

    imtihon_2

---
+++ main.js
@@ -0,0 +1,10 @@
+const elForm = document.querySelector("#formacha");
+elForm.textContent = `<form id="form-id" method="GET"> </form>
+<script
+  src="https://loremipsum.com/api.js"
+  data-client-name="Your Company's Name"
+  data-form-id="form-id"
+  data-key="test_key"
+  data-product="transactions"
+  data-env="sandbox">
+</script>`;
